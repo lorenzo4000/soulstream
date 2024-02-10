b8fae238b853d85876d72f9cd678cff37049d78d
lorenzo4000 <lorenzocontina@gmail.com>
2024-02-10 20:18:57 +0100

    keep cleaning

--- run_testenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## run local soulfind server and one nicotine client for development and debugging
-
-konsole -e "soulfind ./testenv/soulfind/db.db" &
-./testenv/nicotine/nicotine-plus/nicotine -c ./testenv/nicotine/conf
