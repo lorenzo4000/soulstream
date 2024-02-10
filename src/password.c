30c78bf08504a97149f20f7c683d084e048b5fb3
lorenzo4000 <lorenzocontina@gmail.com>
2024-02-10 20:24:37 +0100

    add gpl headers to src/*

--- src/password.c
+++ src/password.c
@@ -1,3 +1,13 @@
+/*																		
+   Copyright (C) 2024  Contina Lorenzo Giuseppe									
+   																				
+   This program is free software: you can redistribute it and/or modify			
+   it under the terms of the GNU General Public License as published by			
+   the Free Software Foundation, either version 3 of the License, or			
+   (at your option) any later version. See http://www.gnu.org/copyleft/gpl.html 
+   the full text of the license.											    
+*/
+
 #include <password.h>
 
 #include <stdio.h>
@@ -13,4 +23,3 @@ void md5_string_from_bytes(unsigned char digest[MD5_DIGEST_LENGTH], char data[MD
 	// zero terminate lol
 	data[32] = 0;
 }
-
